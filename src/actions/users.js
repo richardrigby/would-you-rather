fbbea6c0b4eeda842b2a72cb767d0a72a4b1b240
Richard Rigby <rrig004@gmail.com>
2018-06-07 08:19:56 +1200

    feat: Add user actions for updating questions and votes

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,8 +1,34 @@
 export const RECEIVE_USERS = "RECEIVE_USERS";
+export const RECEIVE_USERS_QUESTIONS = "RECEIVE_USERS_QUESTIONS";
+export const UPDATE_USER_QUESTIONS = "UPDATE_USER_QUESTIONS";
+export const UPDATE_USER_VOTES = "UPDATE_USER_ANSWERS";
 
 export function receiveUsers(users) {
   return {
     type: RECEIVE_USERS,
-    users
+    payload: {
+      users
+    }
+  };
+}
+
+export function updateUserQuestions(question, user) {
+  return {
+    type: UPDATE_USER_QUESTIONS,
+    payload: {
+      author: user,
+      question
+    }
+  };
+}
+
+export function updateUserVotes(qid, user, vote) {
+  return {
+    type: UPDATE_USER_VOTES,
+    payload: {
+      user,
+      qid,
+      vote
+    }
   };
 }
