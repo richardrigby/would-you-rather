d2ced56fd0a445232f9284ccc939171115055f13
Richard Rigby <rrig004@gmail.com>
2018-06-07 08:19:15 +1200

    feat: Add user reducers for updating questions and votes

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,11 +1,36 @@
 import { RECEIVE_USERS } from "../actions/users";
+import { UPDATE_USER_QUESTIONS } from "../actions/users";
+import { UPDATE_USER_VOTES } from "../actions/users";
 
 export default function users(state = {}, action) {
   switch (action.type) {
     case RECEIVE_USERS:
       return {
         ...state,
-        ...action.users
+        ...action.payload.users
+      };
+    case UPDATE_USER_QUESTIONS:
+      let { question, author } = action.payload;
+      author.questions.push(question.id);
+
+      return {
+        ...state,
+        [author.id]: {
+          ...author
+        }
+      };
+
+    case UPDATE_USER_VOTES:
+      const { qid, user, vote } = action.payload;
+      return {
+        ...state,
+        [user]: {
+          ...state[user],
+          answers: {
+            ...state[user].answers,
+            [qid]: vote
+          }
+        }
       };
     default:
       return state;
